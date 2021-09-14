ea995d0864c9015be3b4ee95529195bd87465bca
Samuel Jo <jo.sunghak@gmail.com>
2021-09-13 21:12:14 -0400

    feat/addedTotalAccountValue to BalanceTab

--- src/components/BalancesTab/BalancesTab.js
+++ src/components/BalancesTab/BalancesTab.js
@@ -1,17 +1,33 @@
+import React, { useState, useEffect } from 'react'
 import "./styles.css";
 
 // RANDOM NUMBER GENERATOR
 import { v4 as uuid } from "uuid";
 
 const BalancesTab = ({ currentBalance, networksData }) => {
+  const [totalAccountValue, setTotalAccountValue] = useState('')
+
+  useEffect(() => {
+    let temp = 0;
+    networksData.forEach(data => temp += parseFloat(data.USDC))
+    setTotalAccountValue(temp + currentBalance)
+  }, [])
+
+  const leftTile = (title, balance) => (
+    <div className="balancesTab__leftSide">
+      <span className="balancesTab__title">{title}</span>
+      <h5 className="balancesTab__balance">{`$${balance?.toLocaleString(
+        "en-US"
+      )}`}</h5>
+    </div>
+  );
   return (
     <div className="balancesTab__container">
-      <div className="balancesTab__leftSide">
-        <span className="balancesTab__title">Current Balance</span>
-        <h5 className="balancesTab__balance">{`$${currentBalance?.toLocaleString(
-          "en-US"
-        )}`}</h5>
-      </div>
+      {leftTile("Current Balance", currentBalance)}
+      <span className='balancesTab__toggleOffWhenSmall'>
+        {leftTile("Total Account Value", totalAccountValue)}
+      </span>
+      
       <div className="balancesTab__rightSide">
         <span className="balancesTab__title">Current Holdings</span>
         <div className="balancesTab__rightItems">
