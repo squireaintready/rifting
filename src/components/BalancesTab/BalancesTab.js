bc1283d3d2fd60b6451ab9d6defb860173900dc8
Samuel Jo <jo.sunghak@gmail.com>
2022-02-02 13:28:40 -0500

    refactor/clean code

--- src/components/BalancesTab/BalancesTab.js
+++ src/components/BalancesTab/BalancesTab.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 // RANDOM NUMBER GENERATOR
 import { v4 as uuid } from "uuid";
 
 const BalancesTab = ({ currentBalance, networksData }) => {
-  const [totalAccountValue, setTotalAccountValue] = useState(0)
+  const [totalAccountValue, setTotalAccountValue] = useState(0);
 
   useEffect(() => {
     let temp = 0;
-    networksData?.forEach(data => temp += parseFloat(data.USDC))
-    setTotalAccountValue((temp) + parseFloat(currentBalance))
-  }, [networksData, currentBalance])
+    networksData?.forEach((data) => (temp += parseFloat(data.USDC)));
+    setTotalAccountValue(temp + parseFloat(currentBalance));
+  }, [networksData, currentBalance]);
 
   const leftTile = (title, balance) => (
     <div className="balancesTab__leftSide">
       <span className="balancesTab__title">{title}</span>
       <h5 className="balancesTab__balance">{`$${balance?.toLocaleString(
         "en-US"
-      )}`}</h5>
+      )} ${title}`}</h5>
     </div>
   );
+  
   return (
     <div className="balancesTab__container">
-      {leftTile("Current Balance", currentBalance)}
-      <span className='balancesTab__toggleOffWhenSmall'>
-        {leftTile("Total Account Value", totalAccountValue)}
-      </span>
-      
+      {leftTile("USDC", currentBalance)}
+      <div className="balancesTab__leftSide balancesTab__toggleOffWhenSmall">
+        <span className="balancesTab__title">Total Account Value</span>
+        <h5 className="balancesTab__balance">{`$${totalAccountValue?.toLocaleString(
+          "en-US"
+        )}`}</h5>
+      </div>
+
       <div className="balancesTab__rightSide">
         <span className="balancesTab__title">Current Holdings</span>
         <div className="balancesTab__rightItems">
